ede1f162ec7c0f4e3928a8e0f28085eb82d09f21
Pames <jamespollard12@gmail.com>
2021-06-19 13:14:34 -0400

    fifth commit

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+
+
+export default class CampsiteInfoComponent extends Component {
+    renderCampsite(campsite) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                       <CardTitle>{campsite.name}</CardTitle>
+                       <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    render() {
+
+        console.log(this.props.campsite);
+
+        if (this.props.campsite) {
+            return (
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                </div>
+            );
+        }
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+
